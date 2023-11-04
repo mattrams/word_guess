7c836a96ad960490ddc1238bee3a5cc23ebbceda
Matthew <mgramsarup@gmail.com>
2023-11-04 15:02:41 -0400

    lots of changes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Word Guess Game 🎮
-This is a simple word guess game made using JavaScript, HTML, and CSS. The game is inspired by the classic hangman game, where you have to guess a word by typing letters before you run out of chances. The game has a fun and colorful interface, and it also ~~plays sound effects and music~~ to enhance the gaming experience. 🎵
+This is a simple word guess game made using JavaScript, HTML, and CSS. The game is inspired by the classic hangman game, where you have to guess a word by typing letters before you run out of chances. The game has a fun and colorful interface, and it also plays sound effects and music to enhance the gaming experience. 🎵
 
 ## How to play 🕹️
-To play the game, you need to open the index.html file in your browser. You will see a screen with a random word that is hidden by underscores. You have to type letters on your keyboard in the input box or use the onscreen keyboard to guess the word. If you guess a correct letter, it will be revealed in the word. If you guess a wrong letter, you will lose one chance. You have chances depending on length os word, to guess. If you guess the word before you run out of chances. ~~You can restart the game at any time by clicking the reset button. 🔁~~
+To play the game, you need to open the index.html file in your browser. You will see a screen with a random word that is hidden by underscores. You have to type letters on your keyboard in the input box or use the onscreen keyboard to guess the word. If you guess a correct letter, it will be revealed in the word. If you guess a wrong letter, you will lose one chance. You have chances depending on length os word, to guess. If you guess the word before you run out of chances. You can restart the game at any time by clicking the Play button. 🔁
 
 ## How it works 💻
 The game is powered by JavaScript, which handles the logic and functionality of the game. The game uses an array of words that are randomly selected for each round. The game uses HTML and CSS to create the layout and style of the game. The game uses DOM manipulation to update the elements on the screen based on the user input and the game state.
@@ -22,8 +22,18 @@ The game is powered by JavaScript, which handles the logic and functionality of 
     * A leaderboard that shows your best scores for each difficulty level.
 
 ## Credits 🙏
-The game was created by me, <span style="font-weight: bold; color: rgb(166, 88, 238)">mattrams</span>, who is a JavaScript enthusiast and a web developer. 
+The game was created by me, <b>mattrams</b>, who is a JavaScript enthusiast and a web developer. 
 
 The game was made as a personal project to practice and showcase my skills in JavaScript, HTML, and CSS.
+
+* Speaker icon created by Pixel perfect - <a href="https://www.flaticon.com/free-icons/speaker" title="speaker icons">Flaticon</a>
+
+* Mute icon created by Mayor Icons - <a href="https://www.flaticon.com/free-icons/mute" title="mute icons">Flaticon</a>
+
+* Background Music by <a href="https://pixabay.com/users/dopestuff-30965024/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=128925">dopestuff</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=128925">Pixabay</a>
+
+* Loss Sound Effect and Guess Sound Effects from <a href="https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6008">Pixabay</a>
+
+* Win Sound Effect by <a href="https://pixabay.com/users/universfield-28281460/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=151766">UNIVERSFIELD</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=151766">Pixabay</a>
 
 > Thank you for playing my game! 😊
--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let words = ['abruptly', 'absurd', 'abyss', 'affix', 'askew', 'avenue', 'awkward', 'axiom', 'azure', 'bagpipes', 'bandwagon', 'banjo', 'bayou', 'beekeeper', 'bikini', 'blitz', 'blizzard', 'boggle', 'bookworm', 'boxcar', 'boxful', 'buckaroo', 'buffalo', 'buffoon', 'buxom', 'buzzard', 'buzzing', 'buzzwords', 'caliph', 'cobweb', 'cockiness', 'croquet', 'crypt', 'curacao', 'cycle', 'daiquiri', 'dirndl', 'disavow', 'dizzying', 'duplex', 'dwarves', 'embezzle', 'equip', 'espionage', 'euouae', 'exodus', 'faking', 'fishhook', 'fixable', 'fjord', 'flapjack', 'flopping', 'fluffiness', 'flyby', 'foxglove', 'frazzled', 'frizzled', 'fuchsia', 'funny', 'gabby', 'galaxy', 'galvanize', 'gazebo', 'giaour', 'gizmo', 'glowworm', 'glyph', 'gnarly', 'gnostic', 'gossip', 'grogginess', 'haiku', 'haphazard', 'hyphen', 'iatrogenic', 'icebox', 'injury', 'ivory', 'ivy', 'jackpot', 'jaundice', 'jawbreaker', 'jaywalk', 'jazziest', 'jazzy', 'jelly', 'jigsaw', 'jinx', 'jiujitsu', 'jockey', 'jogging', 'joking', 'jovial', 'joyful', 'juicy', 'jukebox', 'jumbo', 'kayak', 'kazoo', 'keyhole', 'khaki', 'kilobyte', 'kiosk', 'kitsch', 'kiwifruit', 'klutz', 'knapsack', 'larynx', 'lengths', 'lucky', 'luxury', 'lymph', 'marquis', 'matrix', 'megahertz', 'microwave', 'mnemonic', 'mystify', 'naphtha', 'nightclub', 'nowadays', 'numbskull', 'nymph', 'onyx', 'ovary', 'oxidize', 'oxygen', 'pajama', 'peekaboo', 'phlegm', 'pixel', 'pizazz', 'pneumonia', 'polka', 'pshaw', 'psyche', 'puppy', 'puzzling', 'quartz', 'queue', 'quips', 'quixotic', 'quiz', 'quizzes', 'quorum', 'razzmatazz', 'rhubarb', 'rhythm', 'rickshaw', 'schnapps', 'scratch', 'shiv', 'snazzy', 'sphinx', 'spritz', 'squawk', 'staff', 'strength', 'strengths', 'stretch', 'stronghold', 'stymied', 'subway', 'swivel', 'syndrome', 'thriftless', 'thumbscrew', 'topaz', 'transcript', 'transgress', 'transplant', 'triphthong', 'twelfth', 'twelfths', 'unknown', 'unworthy', 'unzip', 'uptown', 'vaporize', 'vixen', 'vodka', 'voodoo', 'vortex', 'voyeurism', 'walkway', 'waltz', 'wave', 'wavy', 'waxy', 'wellspring', 'wheezy', 'whiskey', 'whizzing', 'whomever', 'wimpy', 'witchcraft', 'wizard', 'woozy', 'wristwatch', 'wyvern', 'xylophone', 'yachtsman', 'yippee', 'yoked', 'youthful', 'yummy', 'zephyr', 'zigzag', 'zigzagging', 'zilch', 'zipper', 'zodiac', 'zombie']
-
+let backgroundMusic = document.querySelector('#background-music')
+let isPlaying = true
+let btnSound = document.querySelector('#btn-sound')
+let musicOnIcon = document.querySelector('#btn-sound-on')
+let gameWin = document.querySelector('#game-win')
+let gameLose = document.querySelector('#game-lose')
+let correctBeep = document.querySelector('#correct-beep')
+let wrongBeep = document.querySelector('#wrong-beep')
+let musicOffIcon = document.querySelector('#btn-sound-off')
 let guessesEle = document.querySelector('#guesses')
 let play = document.querySelector('#play')
 let check = document.querySelector('#check')
@@ -20,6 +28,29 @@ let kb1 = ['q','w','e','r','t','y','u','i','o','p']
 let kb2 = ['a','s','d','f','g','h','j','k','l']
 let kb3 = ['z','x','c','v','b','n','m']
 
+function toggleMusic(){
+  if (isPlaying == true){
+    musicOnIcon.style.display = 'none'
+    musicOffIcon.style.display = 'block'
+    wrongBeep.pause()
+    correctBeep.pause()
+    gameLose.pause()
+    gameWin.pause()
+    backgroundMusic.pause()
+    isPlaying = false
+  }
+  else {
+    musicOnIcon.style.display = 'block'
+    musicOffIcon.style.display = 'none'
+    backgroundMusic.play()
+    isPlaying = true
+  }
+}
+
+btnSound.addEventListener('click',()=>{
+  toggleMusic()
+})
+
 function kbrow(arr,usedChrs){
   //let b = '<button>'++'</button>'
   let kbEle = ''
@@ -37,7 +68,6 @@ function kbrow(arr,usedChrs){
 function disableKeyboard(){
   for(btn of keyboardBtns){
     btn.disabled = true
-    console.log(btn)
   }
 }
 
@@ -49,18 +79,36 @@ function checkGuess(ch){
   let correctGuess = false
   for(i in randomWord){
     if(randomWord[i] === inp){
+      if (isPlaying == true) {
+      correctBeep.currentTime = 0
+      correctBeep.play()
+      }
       underWordArr[i] = inp
       correctGuess = true
     }
   }
   unders.innerHTML = "<span>" + underWordArr.join('</span><span>') + "</span>"
   if(!underWordArr.includes('_')){
+    if (isPlaying == true) {
+      wrongBeep.pause()
+      correctBeep.pause()
+      backgroundMusic.pause()
+      gameWin.currentTime = 0
+      gameWin.play()
+      }
     result.style.color = 'green'
     result.innerText = 'You won!'
     check.disabled = true
     disableKeyboard()
   }
   else if(inp === randomWord){
+    if (isPlaying == true) {
+      wrongBeep.pause()
+      correctBeep.pause()
+      backgroundMusic.pause()
+      gameWin.currentTime = 0
+      gameWin.play()
+      }
     unders.innerHTML = "<span>" + inp.split('').join('</span><span>') + "</span>"
     result.style.color = 'green'
     result.innerText = 'You won!'
@@ -68,6 +116,13 @@ function checkGuess(ch){
     disableKeyboard()
   }
   else if(guesses === 1){
+    if (isPlaying == true) {
+      wrongBeep.pause()
+      correctBeep.pause()
+      backgroundMusic.pause()
+      gameLose.currentTime = 0
+      gameLose.play()
+      }
     result.style.color = 'red'
     result.innerText = 'You lose!'
     check.disabled = true
@@ -76,6 +131,10 @@ function checkGuess(ch){
     unders.innerText = 'The correct word is: ' + randomWord.split('').join(' ')
   }
   else if(correctGuess === false){
+    if (isPlaying == true) {
+    wrongBeep.currentTime = 0
+    wrongBeep.play()
+    }
     guesses = guesses - 1
     guessesEle.innerText = 'Guesses remaining: ' + guesses
   }
@@ -89,6 +148,10 @@ function createKeyboard(){
 }
 
 play.addEventListener('click',()=>{
+  if (isPlaying == true) {
+  backgroundMusic.play()
+  backgroundMusic.volume = 0.2
+  }
   underWordArr = [];
   usedChrs = [];
   createKeyboard()
